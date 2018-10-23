1cdf61c60473cb07ab0a7b56886ecb297548fe3f
sunfong <sunfong.lai@gmail.com>
2018-10-23 16:15:18 +0800

    初定文件结构

---
+++ src/main/java/com/sunfong/plf/PlfApplication.java
@@ -0,0 +1,12 @@
+package com.sunfong.plf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlfApplication.class, args);
+    }
+}
