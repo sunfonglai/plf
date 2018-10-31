01d08e00fbdee698ccb402d21c5e29fe0f42679c
sunfong <sunfong.lai@gmail.com>
2018-10-31 17:41:24 +0800

    请求菜单返回树形结构

--- src/main/java/com/sunfong/plf/service/impl/PlfMenuServiceImpl.java
+++ src/main/java/com/sunfong/plf/service/impl/PlfMenuServiceImpl.java
@@ -5,7 +5,10 @@ import com.sunfong.plf.service.itf.IPlfMenuService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author wfr14
@@ -18,6 +21,40 @@ public class PlfMenuServiceImpl implements IPlfMenuService {
 
     @Override
     public List getMenuByRoleId(String roleId) {
-        return plfMenuMapper.selectByRoleId(roleId);
+        List menuList = plfMenuMapper.selectByRoleId(roleId);
+        //再逆向排序
+        List treeList = tranfromMenuToTree(menuList);
+//        Collections.reverse( tranfromMenuToTree(treeList));
+        return treeList;
+    }
+
+    /**
+     * 将列表状的数据改为树形结构
+     * @param menuList
+     * @return 返回树形List 树为3层List结构
+     */
+    private List tranfromMenuToTree(List menuList){
+        List treeList = new ArrayList();
+//      逆向排序后倒插
+        List level2List = new ArrayList();
+        List level3List = new ArrayList();
+        for (int i = 0; i < menuList.size() ; i++) {
+            Map menu = (Map)menuList.get(i);
+            String menuLevel = menu.get("MENU_LEVEL").toString();
+            if ("3".equals(menuLevel)){
+                level3List.add(menu);
+            }
+            if ("2".equals(menuLevel)){
+                menu.put("CHILD_LIST",level3List);
+                level2List.add(menu);
+                level3List = new ArrayList();
+            }
+            if ("1".equals(menuLevel)){
+                menu.put("CHILD_LIST",level2List);
+                treeList.add(menu);
+                level2List = new ArrayList();
+            }
+        }
+        return treeList;
     }
 }
